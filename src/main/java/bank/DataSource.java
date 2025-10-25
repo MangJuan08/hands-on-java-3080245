92d796d75b6cc4c69e9612fcbb2841cc01b2285a
John Edward Bautista <72972235+MangJuan08@users.noreply.github.com>
2025-10-25 01:32:26 +0000

    db connection with query

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -19,7 +19,53 @@ public class DataSource {
     return connection;
   }
 
+  public static void getAccounts() {
+    String sql = "SELECT * FROM accounts a where a.id = 10385";
+
+    try (
+        Connection conn = getConnection();
+        java.sql.Statement stmt = conn.createStatement();
+        java.sql.ResultSet rs = stmt.executeQuery(sql)) {
+
+      // loop through the result set
+      while (rs.next()) {
+        System.out.println(
+            rs.getInt("id") + "\t" +
+                rs.getDouble("balance") + "\t" +
+                rs.getString("type"));
+      }
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public static void getCustomers() {
+    String sql = "SELECT * FROM customers c where c.id = 1109";
+
+    try (
+        Connection conn = getConnection();
+        java.sql.Statement stmt = conn.createStatement();
+        java.sql.ResultSet rs = stmt.executeQuery(sql)) {
+
+      if (rs.next()) {
+        while (rs.next()) {
+          System.out.println(
+              rs.getInt("id") + "\t" +
+                  rs.getString("name") + "\t" +
+                  rs.getString("username") + "\t" +
+                  rs.getString("password") + "\t" +
+                  rs.getString("account_id"));
+        }
+      } else {
+        System.out.println("No data found.");
+      }
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
   public static void main(String[] args) {
-    getConnection();
+    getCustomers();
   }
 }
